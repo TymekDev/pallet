28fa7dac2b51f89f43ef79c525a76aae806750d8
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-04-25 20:36:12 +0200

    feat: implement basic working .create and $

---
+++ R/pallet.R
@@ -0,0 +1,42 @@
+#' @export
+`$.pallet` <- function(pallet, name) {
+  if (!(name %in% names(pallet))) {
+    stop(sprintf("name '%s' not found in '%s'", name, attr(pallet, "name")))
+  }
+
+  get(name, envir = pallet)
+}
+
+#' @export
+print.pallet <- function(x, ...) {
+  cat(paste0("<", attr(x, "name"), ">\n"))
+  invisible(x)
+}
+
+.create <- function(pallet_dir) {
+  pallet_name <- basename(pallet_dir)
+  pallet_file_paths <- list.files(pallet_dir, "\\.R$", full.names = TRUE)
+
+  # Load everything into a "private" namespace environment
+  ns <- new.env(parent = baseenv())
+  for (path in pallet_file_paths) {
+    # TODO: this seems like a silly idea...
+    eval(parse(text = readLines(path)), ns)
+  }
+
+  # Reassign public objects to the public pallet environment
+  env <- new.env(parent = emptyenv())
+  for (name in names(ns)) {
+    if (substr(name, 1, 1) == ".") next
+    env[[name]] <- ns[[name]]
+    lockBinding(name, env)
+  }
+  lockEnvironment(env)
+
+  structure(
+    env,
+    class = "pallet",
+    name = paste0("pallet:", pallet_name),
+    namespace = ns
+  )
+}
